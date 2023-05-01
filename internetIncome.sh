4f3e8bd6a9d1ddd42d511ba898234d551d711ff6
engageub <103608400+engageub@users.noreply.github.com>
2023-05-01 23:20:46 +0530

    Update internetIncome.sh
    Added script link in the comment section

--- internetIncome.sh
+++ internetIncome.sh
@@ -5,7 +5,7 @@
 # Description: This script lets you earn passive income by sharing your internet #
 # connection. It also supports multiple proxies with multiple accounts.          #
 # Script Name: Internet Income (Supports Proxies)                                # 
-#                                                                                #
+# Script Link: https://github.com/engageub/InternetIncome                        #
 # DISCLAIMER: This script is provided "as is" and without warranty of any kind.  #
 # The author makes no warranties, express or implied, that this script is free of#
 # errors, defects, or suitable for any particular purpose. The author shall not  #
