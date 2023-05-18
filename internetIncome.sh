670cf469d173380010499c78bd2288bd5264c818
engageub <103608400+engageub@users.noreply.github.com>
2023-05-18 18:51:40 +0530

    Update internetIncome.sh
    Added ebesucher and mysterium

--- internetIncome.sh
+++ internetIncome.sh
@@ -29,7 +29,23 @@ proxies_file="proxies.txt"
 containers_file="containers.txt"
 earnapp_file="earnapp.txt"
 networks_file="networks.txt"
+mysterium_file="mysterium.txt"
+ebesucher_file="ebesucher.txt"
+firefox_containers_file="firefoxcontainers.txt"
 bitping_folder=".bitping"
+firefox_data_folder="firefoxdata"
+firefox_profile_data="firefoxprofiledata"
+restart_firefox="restartFirefox.sh"
+
+files_to_be_removed=($containers_file $earnapp_file $networks_file $mysterium_file $ebesucher_file $firefox_containers_file)
+folders_to_be_removed=($bitping_folder $firefox_data_folder)
+
+container_pulled=false
+
+# Mysterium and ebesucher first port
+mysterium_first_port=2000
+ebesucher_first_port=3000
+
 
 #Unique Id
 RANDOM=$(date +%s)
@@ -68,6 +84,36 @@ login_bitping() {
   fi
 }
 
+# Define a function to check for open ports
+check_open_ports() {
+  local first_port=$1
+  local num_ports=$2
+
+  port_range=$(seq $first_port $((first_port+num_ports-1)))
+
+  open_ports=0
+  for port in $port_range; do
+    nc -z localhost $port > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      open_ports=$((open_ports+1))
+    fi
+  done
+
+  while [ $open_ports -gt 0 ]; do
+    first_port=$((first_port+num_ports))
+    port_range=$(seq $first_port $((first_port+num_ports-1)))
+    open_ports=0
+    for port in $port_range; do
+      nc -z localhost $port > /dev/null 2>&1
+      if [ $? -eq 0 ]; then
+        open_ports=$((open_ports+1))
+      fi
+    done
+  done
+
+  echo $first_port
+}
+
 # Start all containers 
 start_containers() {
 
@@ -83,10 +129,35 @@ start_containers() {
 
   if [[ $i && $proxy ]]; then
     NETWORK_TUN="--network=container:tun$UNIQUE_ID$i"
+		
+	if [ "$MYSTERIUM" = true ]; then
+	  mysterium_first_port=$(check_open_ports $mysterium_first_port 1)
+	  if ! expr "$mysterium_first_port" : '[[:digit:]]*$' >/dev/null; then
+         echo -e "${RED}Problem assigning port $mysterium_first_port ..${NOCOLOUR}"
+		 echo -e "${RED}Failed to start Mysterium node. Resolve or disable Mysterium to continue. Exiting..${NOCOLOUR}"
+         exit 1
+      fi
+	  mysterium_port="-p $mysterium_first_port:4449 "
+	fi
+	
+	if [[ $EBESUCHER_USERNAME ]]; then
+	  ebesucher_first_port=$(check_open_ports $ebesucher_first_port 1)
+	  if ! expr "$ebesucher_first_port" : '[[:digit:]]*$' >/dev/null; then
+         echo -e "${RED}Problem assigning port $ebesucher_first_port ..${NOCOLOUR}"
+		 echo -e "${RED}Failed to start Ebesucher. Resolve or disable Ebesucher to continue. Exiting..${NOCOLOUR}"
+         exit 1
+      fi
+	  ebesucher_port="-p $ebesucher_first_port:5800 "
+	fi
+	combined_ports=$mysterium_port$ebesucher_port
+	
     # Starting tun containers
-    if CONTAINER_ID=$(sudo docker run --name tun$UNIQUE_ID$i $LOGS_PARAM --restart=always -e LOGLEVEL=$TUN_LOG_PARAM -e PROXY=$proxy -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN -d xjasonlyu/tun2socks); then
+	if [ "$container_pulled" = false ]; then
+      sudo docker pull xjasonlyu/tun2socks  
+    fi
+    if CONTAINER_ID=$(sudo docker run --name tun$UNIQUE_ID$i $LOGS_PARAM --restart=always -e LOGLEVEL=$TUN_LOG_PARAM -e PROXY=$proxy -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN $combined_ports -d xjasonlyu/tun2socks); then
       echo "$CONTAINER_ID" |tee -a $containers_file
-      sudo docker exec $CONTAINER_ID sh -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf;ip rule add iif lo ipproto udp dport 53 lookup main;ip rule add iif lo ipproto udp dport 3478 lookup main'
+	  sudo docker exec $CONTAINER_ID sh -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf;ip rule add iif lo ipproto udp dport 53 lookup main;'
     else
       echo -e "${RED}Failed to start container for proxy. Exiting..${NOCOLOUR}"
       exit 1
@@ -94,112 +165,232 @@ start_containers() {
     sleep 1
   fi
   
+  # Starting Mysterium container
+  if [ "$MYSTERIUM" = true ]; then
+    echo -e "${GREEN}Starting Mysterium container..${NOCOLOUR}"
+    echo -e "${GREEN}Copy the following node url and paste in your browser${NOCOLOUR}"
+    echo -e "${GREEN}You will also find the urls in the file $mysterium_file in the same folder${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull mysteriumnetwork/myst:latest  
+    fi
+    if [[  ! $proxy ]]; then
+	  myst_port="-p $mysterium_first_port:4449"
+	fi
+    if CONTAINER_ID=$(sudo docker run -d --cap-add=NET_ADMIN $NETWORK_TUN $LOGS_PARAM -v myst-data$UNIQUE_ID$i:/var/lib/mysterium-node --restart unless-stopped $myst_port mysteriumnetwork/myst:latest service --agreed-terms-and-conditions); then
+      echo "$CONTAINER_ID" |tee -a $containers_file
+      echo "http://127.0.0.1:$mysterium_first_port" |tee -a $mysterium_file
+      mysterium_first_port=`expr $mysterium_first_port + 1`
+    else
+      echo -e "${RED}Failed to start container for Mysterium..${NOCOLOUR}"
+    fi
+  else
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}Mysterium Node is not enabled. Ignoring Mysterium..${NOCOLOUR}"
+    fi
+  fi
+  
+  # Starting Ebesucher container
+  if [[ $EBESUCHER_USERNAME ]]; then
+    echo -e "${GREEN}Starting Ebesucher container..${NOCOLOUR}"
+    echo -e "${GREEN}Copy the following node url and paste in your browser if required..${NOCOLOUR}"
+    echo -e "${GREEN}You will also find the urls in the file $ebesucher_file in the same folder${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull jlesage/firefox
+      
+      # Exit, if restart script is missing
+      if [ ! -f "$PWD/$restart_firefox" ];then
+        echo -e "${RED}Firefox restart script does not exist. Exiting..${NOCOLOUR}"
+        exit 1
+      fi 
+      
+      # Exit, if firefox profile data is missing
+      if [ ! -d "$PWD/$firefox_profile_data" ];then
+        echo -e "${RED}Firefox Data folder does not exist. Exiting..${NOCOLOUR}"
+        exit 1
+      fi
+      
+      if CONTAINER_ID=$(sudo docker run -d $LOGS_PARAM --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v $PWD:/firefox docker:18.06.2-dind /bin/sh -c 'apk add --no-cache bash && cd /firefox && chmod +x /firefox/restartFirefox.sh && while true; do sleep 3600; /firefox/restartFirefox.sh; done'); then
+        echo "Firefox restart container started"
+        echo "$CONTAINER_ID" |tee -a $containers_file
+      else
+        echo -e "${RED}Failed to start container for ebesucher firefox restart..${NOCOLOUR}"
+        exit 1
+      fi
+    fi
+        
+    # Create folder and copy files
+    mkdir -p $PWD/$firefox_data_folder/data$i
+    sudo chmod -R 777 $PWD/$firefox_profile_data
+    cp -r $PWD/$firefox_profile_data/* $PWD/$firefox_data_folder/data$i/
+    sudo chmod -R 777 $PWD/$firefox_data_folder/data$i
+    if [[  ! $proxy ]]; then
+        eb_port="-p $ebesucher_first_port:5800"
+    fi
+    if CONTAINER_ID=$(sudo docker run -d $NETWORK_TUN $LOGS_PARAM --restart=always -e FF_OPEN_URL="https://www.ebesucher.com/surfbar/$EBESUCHER_USERNAME" -v $PWD/$firefox_data_folder/data$i:/config:rw $eb_port jlesage/firefox); then
+      echo "$CONTAINER_ID" |tee -a $containers_file
+      echo "$CONTAINER_ID" |tee -a $firefox_containers_file
+      echo "http://127.0.0.1:$ebesucher_first_port" |tee -a $ebesucher_file
+      ebesucher_first_port=`expr $ebesucher_first_port + 1`      
+    else
+      echo -e "${RED}Failed to start container for Ebesucher..${NOCOLOUR}"
+    fi
+  else
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}Ebesucher username is not configured. Ignoring Ebesucher..${NOCOLOUR}"
+    fi
+  fi
+  
   # Starting BitPing container
   if [ "$BITPING" = true ]; then
     echo -e "${GREEN}Starting Bitping container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull --platform=linux/amd64 bitping/bitping-node:latest  
+    fi 
     if CONTAINER_ID=$(sudo docker run -d --restart=always --platform=linux/amd64 $NETWORK_TUN $LOGS_PARAM --mount type=bind,source="$PWD/$bitping_folder/",target=/root/.bitping bitping/bitping-node:latest); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for BitPing..${NOCOLOUR}"
     fi
   else
-    echo -e "${RED}BitPing Node is not enabled. Ignoring BitPing..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}BitPing Node is not enabled. Ignoring BitPing..${NOCOLOUR}"
+    fi
   fi
 
   # Starting Repocket container
   if [[ $REPOCKET_EMAIL && $REPOCKET_API ]]; then
     echo -e "${GREEN}Starting Repocket container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull repocket/repocket
+    fi
     if CONTAINER_ID=$(sudo docker run -d --restart=always $NETWORK_TUN $LOGS_PARAM -e RP_EMAIL=$REPOCKET_EMAIL -e RP_API_KEY=$REPOCKET_API repocket/repocket); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for Repocket..${NOCOLOUR}"
     fi
   else
-    echo -e "${RED}Repocket Email or Api is not configured. Ignoring Repocket..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}Repocket Email or Api is not configured. Ignoring Repocket..${NOCOLOUR}"
+    fi
   fi
 
   # Starting Traffmonetizer container
   if [[ $TRAFFMONETIZER_TOKEN ]]; then
     echo -e "${GREEN}Starting Traffmonetizer container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull --platform=linux/amd64 traffmonetizer/cli
+    fi
     if CONTAINER_ID=$(sudo  docker run -d --platform=linux/amd64 --restart=always $LOGS_PARAM $NETWORK_TUN traffmonetizer/cli start accept --token $TRAFFMONETIZER_TOKEN); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for Traffmonetizer..${NOCOLOUR}"
     fi
   else
-    echo -e "${RED}Traffmonetizer Token is not configured. Ignoring Traffmonetizer..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}Traffmonetizer Token is not configured. Ignoring Traffmonetizer..${NOCOLOUR}"
+    fi
   fi
 
   # Starting ProxyRack container
   if [[ $PROXY_RACK_API ]]; then
     echo -e "${GREEN}Starting ProxyRack container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull --platform=linux/amd64 proxyrack/pop
+    fi
     if CONTAINER_ID=$(sudo docker run -d --platform=linux/amd64 $NETWORK_TUN $LOGS_PARAM --restart=always -e api_key=$PROXY_RACK_API -e device_name=$DEVICE_NAME$i proxyrack/pop); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for ProxyRack..${NOCOLOUR}"
   fi  
   else
-    echo -e "${RED}ProxyRack Api is not configured. Ignoring ProxyRack..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}ProxyRack Api is not configured. Ignoring ProxyRack..${NOCOLOUR}"
+    fi
   fi
 
   # Starting IPRoyals pawns container
   if [[ $IPROYALS_EMAIL && $IPROYALS_PASSWORD ]]; then
     echo -e "${GREEN}Starting IPRoyals container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull --platform=linux/amd64 iproyal/pawns-cli:latest
+    fi
     if CONTAINER_ID=$(sudo docker run -d --restart=always $LOGS_PARAM $NETWORK_TUN iproyal/pawns-cli:latest -email=$IPROYALS_EMAIL -password=$IPROYALS_PASSWORD -device-name=$DEVICE_NAME$i -device-id=$DEVICE_NAME$i -accept-tos); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for IPRoyals..${NOCOLOUR}"
     fi   
   else
-    echo -e "${RED}IPRoyals Email or Password is not configured. Ignoring IPRoyals..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}IPRoyals Email or Password is not configured. Ignoring IPRoyals..${NOCOLOUR}"
+    fi
   fi
   
   # Starting Honeygain container
   if [[ $HONEYGAIN_EMAIL && $HONEYGAIN_PASSWORD ]]; then
     echo -e "${GREEN}Starting Honeygain container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull --platform=linux/amd64 honeygain/honeygain    
+    fi
     if CONTAINER_ID=$(sudo docker run -d $NETWORK_TUN $LOGS_PARAM --restart=always --platform=linux/amd64 honeygain/honeygain -tou-accept -email $HONEYGAIN_EMAIL -pass $HONEYGAIN_PASSWORD -device $DEVICE_NAME$i); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for Honeygain..${NOCOLOUR}"
   fi
   else
-    echo -e "${RED}Honeygain Email or Password is not configured. Ignoring Honeygain..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}Honeygain Email or Password is not configured. Ignoring Honeygain..${NOCOLOUR}"
+    fi
   fi
 
   # Starting Peer2Profit container
   if [[ $PEER2PROFIT_EMAIL ]]; then
     echo -e "${GREEN}Starting Peer2Profit container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull peer2profit/peer2profit_linux:latest     
+    fi
     if CONTAINER_ID=$(sudo docker run -d $NETWORK_TUN $LOGS_PARAM --restart always -e P2P_EMAIL=$PEER2PROFIT_EMAIL peer2profit/peer2profit_linux:latest); then
       echo "$CONTAINER_ID" |tee -a $containers_file
     else
       echo -e "${RED}Failed to start container for Peer2Profit..${NOCOLOUR}"
     fi   
   else
-    echo -e "${RED}Peer2Profit Email is not configured. Ignoring Peer2Profit..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}Peer2Profit Email is not configured. Ignoring Peer2Profit..${NOCOLOUR}"
+    fi
   fi
 
   # Starting PacketStream container
   if [[ $PACKETSTREAM_CID ]]; then
     echo -e "${GREEN}Starting PacketStream container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull packetstream/psclient:latest     
+    fi    
     if CONTAINER_ID=$(sudo docker run -d $NETWORK_TUN $LOGS_PARAM --restart always -e CID=$PACKETSTREAM_CID -e http_proxy=$proxy -e https_proxy=$proxy packetstream/psclient:latest); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for PacketStream..${NOCOLOUR}"
     fi   
   else
-    echo -e "${RED}PacketStream CID is not configured. Ignoring PacketStream..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}PacketStream CID is not configured. Ignoring PacketStream..${NOCOLOUR}"
+    fi
   fi
 
   # Starting Proxylite container
   if [[ $PROXYLITE_USER_ID ]]; then
     echo -e "${GREEN}Starting Proxylite container..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull proxylite/proxyservice     
+    fi
     if CONTAINER_ID=$(sudo docker run -d --platform=linux/amd64 $NETWORK_TUN $LOGS_PARAM  -e USER_ID=$PROXYLITE_USER_ID --restart=always proxylite/proxyservice); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for Proxylite..${NOCOLOUR}"
     fi 
   else
-    echo -e "${RED}Proxylite is not configured. Ignoring Proxylite..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}Proxylite is not configured. Ignoring Proxylite..${NOCOLOUR}"
+    fi
   fi
 
   # Starting Earnapp container
@@ -211,15 +402,21 @@ start_containers() {
     RANDOM_ID="$(echo -n "$RANDOM" | md5sum | cut -c1-32)"
     date_time=`date "+%D %T"`
     printf "$date_time https://earnapp.com/r/sdk-node-%s\n" "$RANDOM_ID" | tee -a $earnapp_file
+    if [ "$container_pulled" = false ]; then
+      sudo docker pull --platform=linux/amd64 fazalfarhan01/earnapp:lite
+    fi
     if CONTAINER_ID=$(sudo docker run -d --platform=linux/amd64 $LOGS_PARAM --restart=always $NETWORK_TUN -e EARNAPP_UUID=sdk-node-$RANDOM_ID fazalfarhan01/earnapp:lite); then
       echo "$CONTAINER_ID" |tee -a $containers_file 
     else
       echo -e "${RED}Failed to start container for Earnapp..${NOCOLOUR}"
     fi  
   else
-    echo -e "${RED}Earnapp is not enabled. Ignoring Earnapp..${NOCOLOUR}"
+    if [ "$container_pulled" = false ]; then
+      echo -e "${RED}Earnapp is not enabled. Ignoring Earnapp..${NOCOLOUR}"
+    fi
   fi
-
+  
+  container_pulled=true
 } 
 
 if [[ "$1" == "--start" ]]; then
@@ -229,30 +426,26 @@ if [[ "$1" == "--start" ]]; then
     echo -e "${RED}Properties file $properties_file does not exist, exiting..${NOCOLOUR}"
     exit 1
   fi
-  
-  # Check if containers file exists
-  if [ -f "$containers_file" ]; then
-    echo -e "${RED}Containers file $containers_file still exists, there might be containers still running. Please stop them and delete before running the script. Exiting..${NOCOLOUR}"
-	echo -e "To stop and delete containers run the following command\n"
-	echo -e "${YELLOW}sudo bash internetIncome.sh --delete${NOCOLOUR}\n"
+   
+  for file in "${files_to_be_removed[@]}"
+  do
+  if [ -f "$file" ]; then
+    echo -e "${RED}File $file still exists, there might be containers still running. Please stop them and delete before running the script. Exiting..${NOCOLOUR}"
+    echo -e "To stop and delete containers run the following command\n"
+    echo -e "${YELLOW}sudo bash internetIncome.sh --delete${NOCOLOUR}\n"
     exit 1
   fi
+  done
   
-  # Check if networks file exists
-  if [ -f "$networks_file" ]; then
-    echo -e "${RED}Networks file $networks_file still exists, there might be networks still running. Please stop them and delete  before running the script. Exiting..${NOCOLOUR}"
-	echo -e "To stop and delete containers run the following command\n"
-	echo -e "${YELLOW}sudo bash internetIncome.sh --delete${NOCOLOUR}\n"
+  for folder in "${folders_to_be_removed[@]}"
+  do
+  if [ -d "$folder" ]; then
+    echo -e "${RED}Folder $folder still exists, there might be containers still running. Please stop them and delete before running the script. Exiting..${NOCOLOUR}"
+    echo -e "To stop and delete containers run the following command\n"
+    echo -e "${YELLOW}sudo bash internetIncome.sh --delete${NOCOLOUR}\n"
     exit 1
   fi
-  
-  # Check if bitping folder still exists
-  if [ -d $bitping_folder ]; then 
-    echo -e "${RED}Bitping folder $bitping_folder still exists. Please stop related containers and delete the folder before running the script. Exiting..${NOCOLOUR}"
-	echo -e "To stop and delete containers run the following command\n"
-	echo -e "${YELLOW}sudo bash internetIncome.sh --delete${NOCOLOUR}\n"
-    exit 1
-  fi
+  done
   
   # Read the properties file and export variables to the current shell
   while IFS='=' read -r key value; do
@@ -283,6 +476,7 @@ if [[ "$1" == "--start" ]]; then
       echo -e "${RED}Proxies file $proxies_file does not exist, exiting..${NOCOLOUR}"
       exit 1
     fi
+    
     i=0;
     while IFS= read -r line || [ -n "$line" ]; do
       if [[ "$line" =~ ^[^#].* ]]; then
@@ -292,7 +486,7 @@ if [[ "$1" == "--start" ]]; then
     done < $proxies_file
   else
     echo -e "${RED}USE_PROXIES is disabled, using direct internet connection..${NOCOLOUR}" 
-    start_containers
+    start_containers 
   fi
 fi
 
@@ -313,11 +507,7 @@ if [[ "$1" == "--delete" ]]; then
     # Delete the container file
     rm $containers_file
   fi
-
-  # Delete earnapp file
-  if [ -f "$earnapp_file" ]; then
-    rm $earnapp_file
-  fi 
+  
   
   # Delete networks
   if [ -f "$networks_file" ]; then
@@ -334,11 +524,20 @@ if [[ "$1" == "--delete" ]]; then
     rm $networks_file
   fi
   
-  # Delete Bitping Directory
-  if [ -d $bitping_folder ]; then 
-    rm -Rf $bitping_folder;
+  for file in "${files_to_be_removed[@]}"
+  do
+  if [ -f "$file" ]; then
+    rm $file
   fi
-
+  done
+  
+  for folder in "${folders_to_be_removed[@]}"
+  do
+  if [ -d "$folder" ]; then
+    rm -Rf $folder;
+  fi
+  done
+  
 fi
 
 if [[ ! "$1" ]]; then
