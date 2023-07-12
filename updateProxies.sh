65ec7d26558a167718c934508dca86c594a656fe
engageub <103608400+engageub@users.noreply.github.com>
2023-07-12 15:11:18 +0530

    Update updateProxies.sh
    Removed dependency of version to match starting container name for new versions

--- updateProxies.sh
+++ updateProxies.sh
@@ -29,7 +29,7 @@ for container in `cat $containers_file`
 do
 if sudo docker inspect $container >/dev/null 2>&1; then
   container_image=`sudo docker inspect --format='{{.Config.Image}}' $container`
-  if [ $container_image == "xjasonlyu/tun2socks:v2.5.0" ]; then
+  if [[ $container_image == "xjasonlyu/tun2socks"* ]]; then
     echo $container | tee -a $tun_containers_file
   fi
 fi
@@ -58,7 +58,7 @@ if [ `cat $tun_containers_file|wc -l` == `cat $updated_proxies_file|wc -l` ]; th
 echo "Updating Proxies"
 while read container_id <&3 && read container_proxy <&4; do
   container_image=`sudo docker inspect --format='{{.Config.Image}}' $container_id`
-  if [ $container_image == "xjasonlyu/tun2socks:v2.5.0" ]; then
+  if [[ $container_image == "xjasonlyu/tun2socks"* ]]; then
     sudo docker exec $container_id sh -c "sed -i \"\#--proxy#s#.*#    --proxy ${container_proxy//\//\\\\/} \\\\\#\" entrypoint.sh"
     sudo docker exec $container_id sh -c "sed -i \"\|exec tun2socks|s#.*#echo 'nameserver 8.8.8.8' > /etc/resolv.conf;ip rule add iif lo ipproto udp dport 53 lookup main;exec tun2socks \\\\\#\" entrypoint.sh"
   fi
