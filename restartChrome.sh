6f9c9313cc98ab4fbda6b0925d763f1f401a09e5
engageub <103608400+engageub@users.noreply.github.com>
2023-09-07 01:57:21 +0530

    Update restartChrome.sh
    Removed fpWallets

--- restartChrome.sh
+++ restartChrome.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 chrome_profile_data="chromeprofiledata"
-fp_wallets_file="fpWallets.js"
 chrome_data_folder="chromedata"
 i=0
 for container in `cat chromecontainers.txt`
@@ -18,8 +17,6 @@ if [ -d "/chrome/chromedata/data" ];then
   chown -R 911:911 /chrome/$chrome_profile_data
   cp -r /chrome/$chrome_profile_data /chrome/$chrome_data_folder/data
   chown -R 911:911 /chrome/$chrome_data_folder/data
-  chmod -R 777 /chrome/$fp_wallets_file
-  cp /chrome/$fp_wallets_file /chrome/$chrome_data_folder/data/$chrome_profile_data
   docker update --restart=always $container
   docker start $container
   exit 1
@@ -36,8 +33,6 @@ rm -rf /chrome/$chrome_data_folder/data$i/*
 chown -R 911:911 /chrome/$chrome_profile_data
 cp -r /chrome/$chrome_profile_data /chrome/$chrome_data_folder/data$i
 chown -R 911:911 /chrome/$chrome_data_folder/data$i
-chmod -R 777 /chrome/$fp_wallets_file
-cp /chrome/$fp_wallets_file /chrome/$chrome_data_folder/data$i/$chrome_profile_data
 docker update --restart=always $container
 docker start $container
 
