808c42900847aeca267a7b69d42dff1a961d2703
engageub <103608400+engageub@users.noreply.github.com>
2023-10-09 11:57:55 +0530

    Update restartAdnade.sh
    Changed delay to 5 minutes

--- restartAdnade.sh
+++ restartAdnade.sh
@@ -18,7 +18,7 @@ if [ -d "/chrome/$chrome_data_folder/data" ];then
   cp -r /chrome/$chrome_profile_data /chrome/$chrome_data_folder/data
   chown -R 911:911 /chrome/$chrome_data_folder/data
   docker update --restart=always $container
-  sleep 600
+  sleep 300
   docker start $container
   exit 1
 fi
@@ -35,7 +35,9 @@ chown -R 911:911 /chrome/$chrome_profile_data
 cp -r /chrome/$chrome_profile_data /chrome/$chrome_data_folder/data$i
 chown -R 911:911 /chrome/$chrome_data_folder/data$i
 docker update --restart=always $container
-sleep 600
+if [ $i == 1 ];then
+  sleep 300
+fi
 docker start $container
 
 done
