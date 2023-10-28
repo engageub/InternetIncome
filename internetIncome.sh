f6ea71569bb9c9380a603b748de38e09dfa2fc3a
engageub <103608400+engageub@users.noreply.github.com>
2023-10-28 11:16:22 +0530

    Update internetIncome.sh
    updated packetstream

--- internetIncome.sh
+++ internetIncome.sh
@@ -469,7 +469,7 @@ start_containers() {
     if [ "$container_pulled" = false ]; then
       sudo docker pull packetstream/psclient:latest     
     fi    
-    if CONTAINER_ID=$(sudo docker run -d --name packetstream$UNIQUE_ID$i $NETWORK_TUN $LOGS_PARAM --restart always -e CID=$PACKETSTREAM_CID packetstream/psclient:latest); then
+    if CONTAINER_ID=$(sudo docker run -d --name packetstream$UNIQUE_ID$i $LOGS_PARAM --restart always -e HTTP_PROXY=$proxy -e HTTPS_PROXY=$proxy -e CID=$PACKETSTREAM_CID packetstream/psclient:latest); then
       echo "$CONTAINER_ID" | tee -a $containers_file 
       echo "packetstream$UNIQUE_ID$i" | tee -a $container_names_file
     else
