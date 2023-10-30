b64df4cf7b86bc4ab82eab50ae017c45eef78981
engageub <103608400+engageub@users.noreply.github.com>
2023-10-30 13:26:34 +0530

    Update internetIncome.sh
    Added proxy after the update

--- internetIncome.sh
+++ internetIncome.sh
@@ -175,7 +175,7 @@ start_containers() {
     if [ "$container_pulled" = false ]; then
       sudo docker pull ghcr.io/engageub/tun2proxy:master
     fi
-    if CONTAINER_ID=$(sudo docker run --name tun$UNIQUE_ID$i $LOGS_PARAM --restart=always -e LOGLEVEL=$TUN_LOG_PARAM -e PROXY=$proxy --sysctl net.ipv6.conf.default.disable_ipv6=0 -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN $combined_ports -d ghcr.io/engageub/tun2proxy:master); then
+    if CONTAINER_ID=$(sudo docker run --name tun$UNIQUE_ID$i $LOGS_PARAM --restart=always -e LOGLEVEL=$TUN_LOG_PARAM --sysctl net.ipv6.conf.default.disable_ipv6=0 -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN $combined_ports -d ghcr.io/engageub/tun2proxy:master --proxy $proxy); then
       echo "$CONTAINER_ID" | tee -a $containers_file
       echo "tun$UNIQUE_ID$i" | tee -a $container_names_file
       sudo docker exec $CONTAINER_ID sh -c 'echo "nameserver 1.1.1.1" > /etc/resolv.conf;echo "nameserver 8.8.8.8" >> /etc/resolv.conf;'
