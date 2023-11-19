37d2b9d1ae44e51a2acfcd96b920a1baed9f6bb2
engageub <103608400+engageub@users.noreply.github.com>
2023-11-19 15:51:37 +0530

    Update generateDeviceIds.sh
    Added earnapp file for testing

--- generateDeviceIds.sh
+++ generateDeviceIds.sh
@@ -3,6 +3,7 @@
 #Script to get device Ids from containers
 containers_file="containernames.txt"
 cloud_collab_file="cloudcollab.txt"
+earn_app_file="testing.txt"
 
 if [ ! -f $containers_file ]; then
   echo "$containers_file file does not exist. Exiting.."
@@ -13,11 +14,18 @@ if [ -f $cloud_collab_file ]; then
   rm $cloud_collab_file
 fi
 
+if [ -f $earn_app_file ]; then
+  rm $earn_app_file
+fi
+
 # Get device Ids from containers
 for container in `cat $containers_file`
 do
   if [[ $container == "cloudcollab"* ]]; then
-        sudo docker exec  $container cat /root/.config/CloudCollab/deviceid | od -A n -v -t x1 | tr -d ' ' | tee -a $cloud_collab_file
+    sudo docker exec  $container cat /root/.config/CloudCollab/deviceid | od -A n -v -t x1 | tr -d ' ' | tee -a $cloud_collab_file
+  fi
+  if [[ $container == "testing"* ]]; then
+    sudo docker exec -it $container earnapp showid | tee -a $earn_app_file
   fi
 done
 
