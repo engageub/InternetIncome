0adaa3bcaadd5118f33503f2f086ef9bc047bc2f
engageub <103608400+engageub@users.noreply.github.com>
2024-01-27 21:42:50 +0530

    Update internetIncome.sh
    Changed DNS to google

--- internetIncome.sh
+++ internetIncome.sh
@@ -170,7 +170,7 @@ start_containers() {
     if CONTAINER_ID=$(sudo docker run --name tun$UNIQUE_ID$i $LOGS_PARAM --restart=always -e LOGLEVEL=$TUN_LOG_PARAM --sysctl net.ipv6.conf.default.disable_ipv6=0 -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN $combined_ports -d ghcr.io/blechschmidt/tun2proxy:v0.1.12 $socks5_dns_option --proxy $proxy); then
       echo "$CONTAINER_ID" | tee -a $containers_file
       echo "tun$UNIQUE_ID$i" | tee -a $container_names_file
-      sudo docker exec $CONTAINER_ID sh -c 'echo "nameserver 1.1.1.1" > /etc/resolv.conf;echo "nameserver 8.8.8.8" >> /etc/resolv.conf;'
+      sudo docker exec $CONTAINER_ID sh -c 'echo "nameserver 8.8.4.4" > /etc/resolv.conf;echo "nameserver 8.8.8.8" >> /etc/resolv.conf;'
     else
       echo -e "${RED}Failed to start container for proxy. Exiting..${NOCOLOUR}"
       exit 1
