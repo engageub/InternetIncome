fec3f09676bff0c778316088fdeb0f1a9d12d7ab
engageub <103608400+engageub@users.noreply.github.com>
2024-03-19 18:17:26 +0530

    Update internetIncome.sh
    Added DNS options

--- internetIncome.sh
+++ internetIncome.sh
@@ -159,10 +159,14 @@ start_containers() {
     fi
 
     if [ "$USE_SOCKS5_DNS" = true ]; then
-       socks5_dns_option="--dns direct"
+       dns_option="--dns direct"
+    elif  [ "$USE_DNS_OVER_HTTPS" = true ]; then
+       dns_option="--dns over-tcp"
+    else
+       dns_option="--dns virtual"
     fi
 
-    if CONTAINER_ID=$(sudo docker run --name tun$UNIQUE_ID$i $LOGS_PARAM --restart=always -e LOGLEVEL=$TUN_LOG_PARAM --sysctl net.ipv6.conf.default.disable_ipv6=0 -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN $combined_ports -d ghcr.io/blechschmidt/tun2proxy:v0.1.12 $socks5_dns_option --proxy $proxy); then
+    if CONTAINER_ID=$(sudo docker run --name tun$UNIQUE_ID$i $LOGS_PARAM --restart=always -e LOGLEVEL=$TUN_LOG_PARAM --sysctl net.ipv6.conf.default.disable_ipv6=0 -v '/dev/net/tun:/dev/net/tun' --cap-add=NET_ADMIN $combined_ports -d ghcr.io/blechschmidt/tun2proxy:v0.1.12 $dns_option --proxy $proxy); then
       echo "$CONTAINER_ID" | tee -a $containers_file
       echo "tun$UNIQUE_ID$i" | tee -a $container_names_file
       sudo docker exec $CONTAINER_ID sh -c 'echo "nameserver 8.8.4.4" > /etc/resolv.conf;echo "nameserver 8.8.8.8" >> /etc/resolv.conf;'
