60421782ec0f43598d278c3d3f0f95bdd7391775
engageub <103608400+engageub@users.noreply.github.com>
2024-04-17 12:38:16 +0530

    Update internetIncome.sh

--- internetIncome.sh
+++ internetIncome.sh
@@ -499,7 +499,7 @@ start_containers() {
   fi
   
   # Starting Honeygain container
-  if [[ $HONEYGAIN_EMAIL && $HONEYGAIN_PASSWORD && !$NETWORK_TUN ]]; then
+  if [[ $HONEYGAIN_EMAIL && $HONEYGAIN_PASSWORD && ! $NETWORK_TUN ]]; then
     echo -e "${GREEN}Starting Honeygain container..${NOCOLOUR}"
     if [ "$container_pulled" = false ]; then
       sudo docker pull honeygain/honeygain    
