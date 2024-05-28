9452dbb2ba91987b17792ecb2357b4510dc3d833
engageub <103608400+engageub@users.noreply.github.com>
2024-05-28 17:39:51 +0530

    Update internetIncome.sh
    Modified code to support quotes in vpns file

--- internetIncome.sh
+++ internetIncome.sh
@@ -183,7 +183,7 @@ execute_docker_command() {
   app_name=${container_parameters[0]}
   container_name=${container_parameters[1]}
   echo -e "${GREEN}Starting $app_name container..${NOCOLOUR}"
-  if CONTAINER_ID=$(sudo docker run -d --name $container_name --restart=always "${container_parameters[@]:2}"); then
+  if CONTAINER_ID=$(eval "sudo docker run -d --name $container_name --restart=always ${container_parameters[@]:2}"); then
     echo "$container_name" | tee -a $container_names_file
     echo "$CONTAINER_ID" | tee -a $containers_file
   else
