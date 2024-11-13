eb2141145c625f6650b30369f9c53e2d5e59fe7a
engageub <103608400+engageub@users.noreply.github.com>
2024-11-13 14:29:28 +0530

    Update internetIncome.sh
    Added delay for grass container to login

--- internetIncome.sh
+++ internetIncome.sh
@@ -465,6 +465,8 @@ start_containers() {
     if CONTAINER_ID=$(sudo docker run -d --name grass$UNIQUE_ID$i --platform=linux/amd64 --restart=always $NETWORK_TUN $LOGS_PARAM $DNS_VOLUME -e GRASS_USERNAME=$GRASS_USERNAME -e GRASS_PASSWORD=$GRASS_PASSWORD trangoul/grass-desktop:latest); then
       echo "$CONTAINER_ID" | tee -a $containers_file
       echo "grass$UNIQUE_ID$i" | tee -a $container_names_file
+      echo "Waiting for 60 seconds for grass container to login.."
+      sleep 60
     else
       echo -e "${RED}Failed to start container for Grass..${NOCOLOUR}"
     fi
