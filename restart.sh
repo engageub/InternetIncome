a5f73a7588e088912dd14f0fe31328c429767c6c
engageub <103608400+engageub@users.noreply.github.com>
2025-06-30 03:53:33 +0530

    Update restart.sh
    Added restartURnetwork parameter to restart script

--- restart.sh
+++ restart.sh
@@ -115,4 +115,11 @@ elif [[ "$1" == "--restartEarnapp" ]]; then
   do
     docker restart $container
   done
+
+elif [[ "$1" == "--restartURnetwork" ]]; then
+  # Restarting URnetwork Nodes
+  for container in `cat containernames.txt | grep ^urnetwork`
+  do
+    docker restart $container
+  done
 fi
