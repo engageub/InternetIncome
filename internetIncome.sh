3308ffe9f28cf42ab0c78dfa5aaf35f9dc52845f
engageub <103608400+engageub@users.noreply.github.com>
2025-09-07 18:20:02 +0530

    Update internetIncome.sh
    Removed Network3 docker image

--- internetIncome.sh
+++ internetIncome.sh
@@ -58,7 +58,6 @@ adnade_data_folder="adnadedata"
 chrome_profile_data="chromeprofiledata"
 chrome_profile_zipfile="chromeprofiledata.zip"
 traffmonetizer_data_folder="traffmonetizerdata"
-network3_data_folder="network3-data"
 titan_data_folder="titan-data"
 proxyrack_file="proxyrack.txt"
 cloudflare_file="cloudflared"
@@ -66,7 +65,7 @@ dns_resolver_file="resolv.conf"
 required_files=($banner_file $properties_file $firefox_profile_zipfile $restart_file $generate_device_ids_file)
 files_to_be_removed=($dns_resolver_file $meson_file $cloudflare_file $container_names_file $subnets_file $networks_file $mysterium_file $ebesucher_file $adnade_file $firefox_containers_file $chrome_containers_file $adnade_containers_file $custom_chrome_file $custom_firefox_file $uprock_file)
 folders_to_be_removed=($firefox_data_folder $firefox_profile_data $adnade_data_folder $chrome_data_folder $chrome_profile_data $earnapp_data_folder)
-back_up_folders=($titan_data_folder $network3_data_folder $bitping_data_folder $urnetwork_data_folder $traffmonetizer_data_folder $mysterium_data_folder $custom_chrome_data_folder $custom_firefox_data_folder)
+back_up_folders=($titan_data_folder $bitping_data_folder $urnetwork_data_folder $traffmonetizer_data_folder $mysterium_data_folder $custom_chrome_data_folder $custom_firefox_data_folder)
 back_up_files=($proxyrack_file $earnapp_file)
 restricted_ports=(1 7 9 11 13 15 17 19 20 21 22 23 25 37 42 43 53 69 77 79 87 95 101 102 103 104 109 110 111 113 115 117 119 123 135 137 139 143 161 179 389 427 465 512 513 514 515 526 530 531 532 540 548 554 556 563 587 601 636 993 995 1719 1720 1723 2049 3659 4045 5060 5061 6000 6566 6665 6666 6667 6668 6669 6697 10080)
 container_pulled=false
@@ -1121,22 +1120,6 @@ start_containers() {
   else
     if [[ "$container_pulled" == false && "$ENABLE_LOGS" == true ]]; then
       echo -e "${RED}Gaganode Token is not configured. Ignoring Gaganode..${NOCOLOUR}"
-    fi
-  fi
-
-  # Starting Network3 container
-  if [[ $NETWORK3_EMAIL ]]; then
-    if [ "$container_pulled" = false ]; then
-      sudo docker pull aron666/network3-ai:latest
-    fi
-    mkdir -p $PWD/$network3_data_folder/data$i
-    sudo chmod -R 777 $PWD/$network3_data_folder/data$i
-    network3_volume="-v $PWD/$network3_data_folder/data$i:/usr/local/etc/wireguard"
-    docker_parameters=($LOGS_PARAM $DNS_VOLUME $MAX_MEMORY_PARAM $MEMORY_RESERVATION_PARAM $MEMORY_SWAP_PARAM $CPU_PARAM $NETWORK_TUN $network3_volume --cap-add NET_ADMIN --device /dev/net/tun -e EMAIL=$NETWORK3_EMAIL aron666/network3-ai:latest)
-    execute_docker_command "Network3" "network3$UNIQUE_ID$i" "${docker_parameters[@]}"
-  else
-    if [[ "$container_pulled" == false && "$ENABLE_LOGS" == true ]]; then
-      echo -e "${RED}Network3 Email is not configured. Ignoring Network3..${NOCOLOUR}"
     fi
   fi
 
