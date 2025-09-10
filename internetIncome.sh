d4442f2f4d1531170cde48d5759f8207d37347f5
engageub <103608400+engageub@users.noreply.github.com>
2025-09-10 16:20:40 +0530

    Update internetIncome.sh
    Updated wipter docker version

--- internetIncome.sh
+++ internetIncome.sh
@@ -741,9 +741,9 @@ start_containers() {
   if [[ $WIPTER_EMAIL && $WIPTER_PASSWORD ]]; then
     echo -e "${GREEN}Starting Wipter container..${NOCOLOUR}"
     if [ "$container_pulled" = false ]; then
-      sudo docker pull --platform=linux/amd64 ghcr.io/adfly8470/wipter/wipter@sha256:0256c4129fb19ff2b0cf9622d9619eaec84f1b8daa750301683590084ab192ca
+      sudo docker pull --platform=linux/amd64 ghcr.io/adfly8470/wipter/wipter@sha256:2c0d86dce03e400c5976897be4a7b4183aec722ee294853e920038483d628b1a
     fi
-    if CONTAINER_ID=$(sudo docker run -d --platform=linux/amd64 --name wipter$UNIQUE_ID$i --restart=always $LOGS_PARAM $DNS_VOLUME $NETWORK_TUN -e WIPTER_EMAIL=$WIPTER_EMAIL -e WIPTER_PASSWORD=$WIPTER_PASSWORD ghcr.io/adfly8470/wipter/wipter@sha256:0256c4129fb19ff2b0cf9622d9619eaec84f1b8daa750301683590084ab192ca); then
+    if CONTAINER_ID=$(sudo docker run -d --platform=linux/amd64 --name wipter$UNIQUE_ID$i --restart=always $LOGS_PARAM $DNS_VOLUME $NETWORK_TUN -e WIPTER_EMAIL=$WIPTER_EMAIL -e WIPTER_PASSWORD=$WIPTER_PASSWORD ghcr.io/adfly8470/wipter/wipter@sha256:2c0d86dce03e400c5976897be4a7b4183aec722ee294853e920038483d628b1a); then
       echo "$CONTAINER_ID" | tee -a $containers_file
       echo "wipter$UNIQUE_ID$i" | tee -a $container_names_file
     else
