eaab96e793245dc5f1b3b21406452774ca8d9ec7
engageub <103608400+engageub@users.noreply.github.com>
2026-05-15 16:40:04 +0530

    Update internetIncome.sh
    updated tun2proxy to v0.8.0

--- internetIncome.sh
+++ internetIncome.sh
@@ -728,7 +728,7 @@ start_containers() {
     elif [ "$USE_TUN2PROXY" = true ];then
       # Starting tun2proxy containers
       if [ "$container_pulled" = false ]; then
-        sudo docker pull ghcr.io/tun2proxy/tun2proxy:v0.7.21
+        sudo docker pull ghcr.io/tun2proxy/tun2proxy:v0.8.0
       fi
       if [[ "$ENABLE_LOGS" != true ]]; then
         TUN_LOG_PARAM="off"
@@ -753,7 +753,7 @@ start_containers() {
           exit 1
         fi
       fi
-      docker_parameters=($HOST_NAME $LOGS_PARAM $MAX_MEMORY_PARAM $MEMORY_RESERVATION_PARAM $MEMORY_SWAP_PARAM $CPU_PARAM $CUSTOM_NETWORK --sysctl net.ipv6.conf.default.disable_ipv6=0 --device /dev/net/tun --cap-add=NET_ADMIN $combined_ports -d ghcr.io/tun2proxy/tun2proxy:v0.7.21 $dns_option --proxy $proxy --verbosity $TUN_LOG_PARAM)
+      docker_parameters=($HOST_NAME $LOGS_PARAM $MAX_MEMORY_PARAM $MEMORY_RESERVATION_PARAM $MEMORY_SWAP_PARAM $CPU_PARAM $CUSTOM_NETWORK --sysctl net.ipv6.conf.default.disable_ipv6=0 --device /dev/net/tun --cap-add=NET_ADMIN $combined_ports -d ghcr.io/tun2proxy/tun2proxy:v0.8.0 $dns_option --proxy $proxy --verbosity $TUN_LOG_PARAM)
       execute_docker_command "Proxy" "tun$UNIQUE_ID$i" "${docker_parameters[@]}"
     else
       # Starting tun2socks containers
@@ -761,7 +761,7 @@ start_containers() {
         if [ "$USE_SOCKS5_DNS" = true ]; then
           sudo docker pull ghcr.io/heiher/hev-socks5-tunnel:main
         elif [[ "$USE_SOCKS5_DNS" != "true" && "$USE_DNS_OVER_HTTPS" != "true" && "$proxy" =~ ^(http|https|socks4|socks5):// ]]; then
-          sudo docker pull ghcr.io/tun2proxy/tun2proxy:v0.7.21
+          sudo docker pull ghcr.io/tun2proxy/tun2proxy:v0.8.0
         elif [[ "$USE_DNS_OVER_HTTPS" == "true" && "$proxy" == socks5://* ]]; then
           sudo docker pull ghcr.io/heiher/hev-socks5-tunnel:main
         else
@@ -827,7 +827,7 @@ start_containers() {
           TUN_LOG_PARAM="trace"
         fi
         dns_option="--dns virtual"
-        docker_parameters=($HOST_NAME $LOGS_PARAM $MAX_MEMORY_PARAM $MEMORY_RESERVATION_PARAM $MEMORY_SWAP_PARAM $CPU_PARAM $CUSTOM_NETWORK --sysctl net.ipv6.conf.default.disable_ipv6=0 --device /dev/net/tun --cap-add=NET_ADMIN $combined_ports -d ghcr.io/tun2proxy/tun2proxy:v0.7.21 $dns_option --proxy $proxy --verbosity $TUN_LOG_PARAM)
+        docker_parameters=($HOST_NAME $LOGS_PARAM $MAX_MEMORY_PARAM $MEMORY_RESERVATION_PARAM $MEMORY_SWAP_PARAM $CPU_PARAM $CUSTOM_NETWORK --sysctl net.ipv6.conf.default.disable_ipv6=0 --device /dev/net/tun --cap-add=NET_ADMIN $combined_ports -d ghcr.io/tun2proxy/tun2proxy:v0.8.0 $dns_option --proxy $proxy --verbosity $TUN_LOG_PARAM)
         execute_docker_command "Proxy" "tun$UNIQUE_ID$i" "${docker_parameters[@]}"
       elif [[ "$USE_DNS_OVER_HTTPS" == "true" && "$proxy" == socks5://* ]]; then
         SOCKS_PROXY=$proxy
